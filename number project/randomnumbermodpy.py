a9fb73e4144051c35c26bde611b10537f0d396f1
Lk10 <62957566+GuestAUser@users.noreply.github.com>
2021-12-26 13:17:33 -0300

    Update randomnumbermodpy.py

--- number project/randomnumbermodpy.py
+++ number project/randomnumbermodpy.py
@@ -6,27 +6,21 @@ import debuganim
 #maybe add multi threading
 
 welcome_msg = "==========================================\nWelcome to the simple Number Guess Game!!!\n=========================================="
-
 games = 0
 points = 0
-
 def restart():
     system('clear')
     print(welcome_msg)
-
 while games < 10:
     try:
         if games > 0:
             print("Points: " + str(points) + ". Game: " + str(games))
-            
         print(welcome_msg)
         games += 1
         print("Try a number (1-25): ") #original = 99;
         Choice = int(input())
         system('clear')
-
         X = random.randrange(1,25,1) #(1, 99, 1)
-        
         print("The number is " +  str(X))
         if X == Choice:
             system('clear')
@@ -37,21 +31,18 @@ while games < 10:
             print("So close!")
             points += 10
         else:
-            print("You miss!")
-            
+            print("You miss!")   
     except (ValueError, TypeError):
         print("---> PLEASE DO NOT USE STRINGS FOR INPUT! <---")
         print("__could be caused by empty input too__")
         input('continue? [ enter ]')
         games -= 1
         restart()
-        
     except KeyboardInterrupt:
         system('clear')
         print("\n KeyBoardExit Triggered, end!")
         print("Code: Lk10 -> GuestAUser")
         exit()
-
 print("Good Game!")
 print("Games: "+ str(games) + ". Points: "+str(points))
 print("Code: Lk10 -> GuestAUser")
