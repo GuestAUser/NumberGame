e20b0b54daca324d23d9a0705f50cabada12ce6c
Artur <46168893+MeEu1@users.noreply.github.com>
2021-12-25 23:22:27 -0300

    Update randomnumbermodpy.py

--- number project/randomnumbermodpy.py
+++ number project/randomnumbermodpy.py
@@ -3,22 +3,28 @@ from os import system #linux case
 from termcolor import colored
 from time import sleep
 import debuganim
+import keyboard
 #maybe add multi threading
 
-print("==========================================")
-print("Welcome to the simple random number say!!!")
-print("==========================================")
+welcome_msg = "==========================================\nWelcome to the simple random number say!!!\n\=========================================="
+
 games = 0
 points = 0
+
+def restart():
+    system('clear')
+    print(welcome_msg)
 
 while games < 10:
     try:
         if games > 0:
             print("Points: " + str(points) + ". Game: " + str(games))
-        games += 1
+        
         print("Try a number (1-25): ") #original = 99;
         Choice = int(input())
+
         X = random.randrange(1,25,1) #(1, 99, 1)
+        
         print("The number is " +  str(X))
         if X == Choice:
             system('clear')
@@ -30,20 +36,21 @@ while games < 10:
             points += 10
         else:
             print("You miss!")
+        
+        games += 1
 
     except ValueError:
         print("---> PLEASE DO NOT USE STRINGS FOR INPUT! <---")
         print("__could be caused by empty input too__")
         input('continue? [ enter ]')
-        games -= 1 
-        system('clear')
-        print("Welcome to the simple random number say!!!")
-        print("Enjoy!")
+        if keyboard.is_pressed('enter'): #must run as root on linux
+            restart()
+        
     except KeyboardInterrupt:
         system('clear')
         print("\n KeyBoardExit Triggered, end!")
         print("Code: Lk10 -> GuestAUser")
-        exit();
+        exit()
 
 print("Good Game!")
 print("Games: "+ str(games) + ". Points: "+str(points))
