6aa63c1e7b4d5193841a6bd7931351d3d43e2d47
Lk10 <62957566+GuestAUser@users.noreply.github.com>
2021-12-28 02:57:15 -0300

    More gui adding
    The animation and border's are hard coded for xterm windows

--- number project/debuganim.py
+++ number project/debuganim.py
@@ -8,19 +8,22 @@ def textanimation(packed):
     y_pushtimes = len(packed)
     format_packed =  str(packed)
     color_sets = [] #future feature?
+    border = "------------------------------------------------------------------------------"
     while x_frame != 18: #18 frames for cycle
         system('clear')
         x_frame += 1
         #print("framecount -> " + str(x_frame) + "\n") #debug status [frames display]
-        #print("y_pustimesvar: " + str(y_pushtimes) + "| To add to Xframes-> " + str(x_frame)) #debug status [push]
+        #print("y_pustimesvar: " + str(y_pushtimes) + "| To add to Xframes-> " + str(x_frame)) #debug status [push and sh1t]
+        print(colored("CONGRATS!".center(80), 'red' , attrs=['bold']))
+        print(border)
         print(colored(format_packed.center(y_pushtimes*int(x_frame)), 'green', attrs=['bold'])) #frame set buffer -> {1}
         print(colored(format_packed.center(y_pushtimes*int(x_frame)), 'red', attrs=['bold'])) #frame set buffer -> {2}
         print(colored(format_packed.center(y_pushtimes*int(x_frame)), 'cyan', attrs=['bold'])) #frame set buffer -> {3}
         print(colored(format_packed.center(y_pushtimes*int(x_frame)), 'yellow', attrs=['bold'])) #frame set buffer -> {4}
         print(colored(format_packed.center(y_pushtimes*int(x_frame)), 'grey', attrs=['bold'])) #frame set buffer -> {5}
         print(colored(format_packed.center(y_pushtimes*int(x_frame)), 'white', attrs=['bold'])) #frame set buffer -> {6}
+        print(border)
         sleep(0.25) #time to reset loop{animation};
-
 #ideia space
 
 What_to_do = '''
